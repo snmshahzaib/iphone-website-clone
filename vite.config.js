51a4d435dbd42eee0767e205cc621ba0d6213be4
Shahzaib Nadeem <61820487+snmshahzaib@users.noreply.github.com>
2024-12-15 14:06:06 +0500

    updates

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), sentryVitePlugin({
-    org: "jsm-x9",
+    org: "codebrisk",
     project: "javascript-react"
   })],
 
